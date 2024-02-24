7589916dfe3fd765f14316d0013cf48c0f6c57dc
Sgt Raphael Mattos <desenvolvimento@ceadex.eb.mil.br>
2024-02-24 12:03:20 -0300

    add bootstrat js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Footer from "@/componentes/footer/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -12,9 +13,11 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {    
+
   return (
     <html lang="en">
-      <head>       
+      <head>  
+        <script src= "/assets/js/bootstrap.min.js" defer/>
       </head>
       <body className={inter.className}>
          {children}
